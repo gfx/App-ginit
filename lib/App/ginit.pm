a8fb8c1e027516269e0730989947b977fb40d91f
gfx <gfuji@cpan.org>
2010-08-23 12:01:29 +0900

    initial commit

---
+++ lib/App/ginit.pm
@@ -0,0 +1,32 @@
+package App::ginit;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::ginit -
+
+=head1 SYNOPSIS
+
+  use App::ginit;
+
+=head1 DESCRIPTION
+
+App::ginit is
+
+=head1 AUTHOR
+
+Goro Fuji (gfx) E<lt>gfuji at cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
